d468afc835d1fb4cacfaa4608486873c1021f223
Vansh44 <iamvanshgupta608@gmail.com>
2026-04-04 02:48:52 +0530

    trigger dev deployment

---
+++ app/src/pages/HomePage/home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Soakd!</h1>
+            <p>Get Soakd, in real ingredients.</p>
+        </div>
+    );      
+}
+
+export default Home;
